15dc2bb398d529765c929cf12252acd9bc9764d4
muny123 <117163499+muny123@users.noreply.github.com>
2024-01-30 02:05:31 +0100

    new

--- Day_four_project/script.js
+++ Day_four_project/script.js
@@ -100,19 +100,19 @@ else{
 // //quest 2
 let month = prompt("Enter the the month today?")
 month = month.toLowerCase()
-if(month == 'September' || month == 'October' || month == 'November') {
+if(month == 'september' || month == 'october' || month == 'november') {
     console.log("The season is Autumn");
 
-} else if(month == 'December' || month == 'January' || month == 'February'){
+} else if(month == 'december' || month == 'january' || month == 'february'){
     console.log("The season is Winter");
 
 }
-else if(month == 'March' || month == 'April' || month == 'May'){
+else if(month == 'march' || month == 'april' || month == 'may'){
     console.log("The season is Spring");
 
 }
-else if(month == 'June' || month == 'July' || month == 'August'){
-    console.log("The season is Summerr");
+else if(month == 'june' || month == 'july' || month == 'august'){
+    console.log("The season is Summer");
 
 }
 else{
